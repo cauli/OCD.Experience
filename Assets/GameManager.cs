935c7bb0e091b1685f73d16bf850757b17eb8921
Guilherme-Note <guilherme.goncalvessantos@gmail.com>
2016-01-30 21:33:43 -0200

    how to screen finished

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+
+	public GameObject gotItBtn;
+	public Text attemptTxt;
+
+	//private int numberAttempts = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +20,18 @@ public class GameManager : MonoBehaviour {
 	}
 
 	public void WonPuzzle () {
+		
+		gotItBtn.SetActive (true);
+
 		Debug.Log ("!!!!! WON");	
 	}
 
 
 	public void LostPuzzle () {
+		// increasing the number of attempts in the UI
+		//numberAttempts++;
+		//attemptTxt.text = numberAttempts.ToString();
+
 		Debug.LogError ("!!!!! LOST, CANT MOVE");
 	}
 }
