9228cf38e508fd2d50b05436d45398f7eaa8f9b0
Cauli Tomaz <caulitomaz@gmail.com>
2016-01-30 01:25:00 -0200

    Initial commit

---
+++ Assets/Grid.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	/*
+	 *  Cada elemento do array corresponde a um ponto, assumindo TOP LEFT 
+	 *  Ex: O primeiro índice [0][0] (P) no array dimensional pega os valores
+	 *    
+	 *        
+	 *         0  
+	 *    P----------
+	 *    |
+	 *    |
+	 *  2 |
+	 *    |
+	 *    |
+	 */
+
+	public int[,] verticalArray  = new int[,]         { { 2, 2, 0 }, 
+												        { 2, 2, 0 },
+												        { 0, 0, 0 } };
+
+	public int[,] horizontalArray  = new int[,]       { { 0, 0, 0 }, 
+												        { 5, 0, 0 },
+												        { 0, 0, 0 } };
+	  
+
+	// 0 = Null
+	// 1 = Player start
+	// 2 = Player end
+	public int[,] posArray         = new int[,]       { { 0, 0, 0 }, 
+													    { 1, 2, 0 },
+													    { 0, 0, 0 } };
+
+	public int lengthOfStick = 10;  // Stick length
+	int l = 0;
+
+
+	/**
+	 *   DEBUGGING 
+	 */
+	public void PrintVertical() {
+		Debug.LogWarning("Vertical");
+
+		string line = "";
+
+		for (int x = 0; x < verticalArray.GetLength(0); x += 1) {
+
+			for (int y = 0; y < verticalArray.GetLength(1); y += 1) {
+				line += verticalArray[x, y];
+			}
+
+			line += "\n";
+		}
+
+		Debug.LogWarning(line);
+	}
+
+	public void PrintHorizontal() {
+		Debug.LogWarning("Horizontal");
+
+		string line = "";
+
+		for (int x = 0; x < horizontalArray.GetLength(0); x += 1) {
+
+
+			for (int y = 0; y < horizontalArray.GetLength(1); y += 1) {
+				line += horizontalArray[x, y];
+			}
+
+			line += "\n";
+		}
+
+		Debug.LogWarning(line);
+	}
+	/**
+	 *   END DEBUGGING 
+	 */
+
+
+ 	// Use this for initialization
+	void Start () {
+		l = lengthOfStick;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		for (int row = 0; row < verticalArray.GetLength (0); row++) {
+			for (int col = 0; col < verticalArray.GetLength (1); col++) {
+				if ((int)verticalArray [row,col] > 0) {
+					Debug.DrawLine (new Vector3 (row*l, col*l, 0), new Vector3 (row*l+ (1*l), col*l , 0), Color.red);
+				}
+			}
+		}
+
+		for (int row = 0; row < horizontalArray.GetLength (0); row++) {
+			for (int col = 0; col < horizontalArray.GetLength (1); col++) {
+				if ((int)horizontalArray [row,col] > 0) {
+					Debug.DrawLine (new Vector3 (row*l, col*l, 0), new Vector3 (row*l, col*l + (1*l), 0), Color.blue);
+				}
+			}
+		}
+
+	}
+
+	public bool isInsideBounds(int col, int row) {
+		if (col < 0 || row < 0) {
+			return false;
+		}
+
+		if (row < verticalArray.GetLength(0) && col < verticalArray.GetLength(1))
+		{
+			return true;
+		}
+		else  
+		{
+			return false;
+		}
+	}
+
+	public bool doMove(int col, int row, bool vertical) {
+		if (vertical) {
+			// Se este elemento for 0, não existem jogadas disponíveis para o usuário
+			if ((int)verticalArray [row, col] <= 0) {
+				return false;
+			} else {
+				verticalArray [row, col] = ((int)verticalArray [row, col]) - 1;
+				return true;
+			}
+
+		} else {
+			// Se este elemento for 0, não existem jogadas disponíveis para o usuário
+			if ((int)horizontalArray [row, col] <= 0) {
+				return false;
+			} else {
+				horizontalArray [row, col] = ((int)horizontalArray [row, col]) - 1;
+				return true;
+			}
+		}
+	}
+}
---
+++ Assets/GridMovable.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridMovable : MonoBehaviour {
+
+	public Grid map;
+
+	public int l = -1;
+
+	int currentX;
+	int currentY;
+
+	enum Way{ Vertical, Horizontal };
+	enum Direction{ Up, Down, Left, Right };
+
+	// Use this for initialization
+	void Start () {
+
+		// Setando o length para o mesmo do Grid PAI
+		l = map.lengthOfStick;
+
+		this.transform.position = GetStartPosition (map);
+	}
+			
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("up"))
+			MoveUp ();
+
+		if (Input.GetKeyDown ("left"))
+			MoveLeft ();
+
+		if (Input.GetKeyDown ("right"))
+			MoveRight ();
+
+		if (Input.GetKeyDown ("down"))
+			MoveDown ();
+	}
+
+
+	bool MoveIfPossible(int destinyX,  int destinyY, int currentX, int currentY, Way way, Direction direction) {
+
+		int posX = -100;
+		int posY = -100;
+
+		/*
+		 *  VER PAPEL 2 PARA EXPLICACAO
+		 * 
+		 * 
+		 */
+		if (direction == Direction.Up || direction == Direction.Left) {
+			posX = destinyX;
+			posY = destinyY;
+		} else {
+			posX = currentX;
+			posY = currentY;
+		}
+
+		// To move
+		if (map.isInsideBounds (destinyX, destinyY)) {
+			Debug.Log ("Destiny Inside bounds X: " + destinyX + " --- " + " Y: " + destinyY + "");
+
+			//if(map.doMove(posX, posY, (way == Way.Vertical) )) {
+
+				map.PrintVertical ();
+				map.PrintHorizontal ();
+
+				this.transform.position = XYtoVector3 (destinyX, destinyY);
+
+				currentX = destinyX;
+				currentY = destinyY;
+			/*}
+			else {
+				Debug.Log ("But cannot move X: " + destinyX + " --- " + " Y: " + destinyY + "");	
+			  return false;
+			}*/
+
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	void MoveUp() {
+		Debug.Log ("Current X : " + currentX + " --- " + " Current Y : " + currentY);
+
+		int destinyY = currentY - 1;
+		int destinyX = currentX;
+
+		if (MoveIfPossible (destinyX, destinyY, currentX, currentY, Way.Vertical, Direction.Up)) {
+			Debug.Log ("Moving to DestinyX : " + destinyX + " --- " + " DestinyY : " + destinyY + " DONE!!!!!!!!");
+		} else {
+			Debug.Log ("Moving to DestinyX : " + destinyX + " --- " + " DestinyY : " + destinyY + " NOT POSSIBLE");
+		}
+
+	}
+		
+	void MoveDown() {
+		Debug.Log ("Current X : " + currentX + " --- " + " Current Y : " + currentY);
+
+		int destinyY = currentY + 1;
+		int destinyX = currentX;
+
+		if (MoveIfPossible (destinyX, destinyY, currentX, currentY, Way.Vertical, Direction.Down)) {
+			Debug.Log ("Moving to DestinyX : " + destinyX + " --- " + " DestinyY : " + destinyY + " DONE!!!!!!!!");
+		} else {
+			Debug.Log ("Moving to DestinyX : " + destinyX + " --- " + " DestinyY : " + destinyY + " NOT POSSIBLE");
+		}
+
+	}
+
+	void MoveLeft() {	
+		Debug.Log ("Current X : " + currentX + " --- " + " Current Y : " + currentY);
+		
+		int destinyY = currentY;
+		int destinyX = currentX-1;
+
+		if (MoveIfPossible (destinyX, destinyY, currentX, currentY, Way.Horizontal, Direction.Left)) {
+			Debug.Log ("Moving to DestinyX : " + destinyX + " --- " + " DestinyY : " + destinyY + " DONE!!!!!!!!");
+		} else {
+			Debug.Log ("Moving to DestinyX : " + destinyX + " --- " + " DestinyY : " + destinyY + " NOT POSSIBLE");
+		}
+
+	}
+
+	void MoveRight() {
+		Debug.Log ("Current X : " + currentX + " --- " + " Current Y : " + currentY);
+
+		int destinyY = currentY;
+		int destinyX = currentX+1;
+
+		if (MoveIfPossible (destinyX, destinyY, currentX, currentY, Way.Horizontal, Direction.Up)) {
+			Debug.Log ("Moving to DestinyX : " + destinyX + " --- " + " DestinyY : " + destinyY + " DONE!!!!!!!!");
+		} else {
+			Debug.Log ("Moving to DestinyX : " + destinyX + " --- " + " DestinyY : " + destinyY + " NOT POSSIBLE");
+		}
+
+	}
+
+
+	/**
+	 *  Returns the specified position of the player by the grid. 
+	 */
+	Vector3 GetStartPosition(Grid map) {
+		for (int row = 0; row < map.posArray.GetLength (0); row++) {
+			for (int col = 0; col < map.posArray.GetLength (1); col++) {
+				if ((int)map.posArray [row,col] == 1) {
+					currentY = row;
+					currentX = col;
+
+					return XYtoVector3 (currentX, currentY);
+				}
+			}
+
+		}
+
+		return Vector3.zero;
+	}
+
+
+	Vector3 XYtoVector3(int col, int row) {
+		return new Vector3 (row * l, col * l, 0);
+	}
+}
