059a952cd744e4eaee5e40ab5e1e3c04473a0e92
Guilherme-Note <guilherme.goncalvessantos@gmail.com>
2016-01-31 12:58:57 -0200

    Let's get funky

---
+++ Assets/Music/MusicManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public GameObject musicObj;
+
+	void Awake() {
+		DontDestroyOnLoad(musicObj);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
